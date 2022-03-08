cc451856f0aaf2bf3d16c3816678d501f87abf5e
raubenw <raubenw@gmail.com>
2022-03-08 12:26:24 +0200

    eerste commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace CSharpAsyncBasic
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("\nStarting the application");
+            DownloadSimulation();
+
+            Console.WriteLine("Press enter to continue...");
+            Console.ReadLine();
+        }
+
+        static void DownloadSimulation()
+        {
+            Task.Run(() =>
+            {
+                Thread.Sleep(5000);
+                Console.WriteLine("Download Complete!");
+            });
+        }
+    }
+}
